11273dfb7d3a2aa55079d740a7e86e1997b80a6b
lahiruCirclebook <lahirucj98@gmail.com>
2023-02-05 18:20:06 +0530

    sss

--- backend/infoglobal/app/Http/Controllers/AuthController.php
+++ backend/infoglobal/app/Http/Controllers/AuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 
+
 class AuthController extends Controller
 {
 
@@ -148,6 +149,63 @@ class AuthController extends Controller
                 'status' => false,
                 'message' => 'error while logged out'
             ]);
+        }
+    }
+
+
+    //add customer
+    public function addCustomer(Request $request)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'nic' => 'required|min:10|max:10|unique',
+                'full_name' => 'required',
+                'address' => 'required',
+                'dob' => 'required',
+                'religions' => 'required',
+                'phone_no' => 'required|max:10',
+                'date_of_registered' => 'required',
+            ], [
+                'nic.required' => 'NIC Filed is Required',
+                'full_name.required' => 'Full Name Filed is Required',
+                'address.required' => 'Address Filed is Required',
+                'dob.required' => 'DoB Filed is Required',
+                'religions.required' => 'Religions Filed is Required',
+                'phone_no' => 'Phone number Filed is Required',
+                'date_of_registered' => 'Date of Registered Filed is Required'
+            ]);
+
+            if ($validator->fails()) {
+
+                $error = $validator->errors()->first();
+
+                return response()->json(['status' => false, 'message' => $error])->setStatusCode(400);
+            }
+
+            if (Customer::where('nic', '=', $request->nic)->exists()) {
+                return response()->json(['status' => false, 'message' => 'customer already exists'])->setStatusCode(400);
+            }
+
+            $customer = new Customer;
+
+            $customer->nic =  $request->nic;
+            $customer->full_name =  $request->full_name;
+            $customer->address =  $request->address;
+            $customer->dob =  $request->dob;
+            $customer->religions =  $request->religions;
+            $customer->phone_no =  $request->phone_no;
+            $customer->date_of_registered =  $request->date_of_registered;
+
+            return $customer->save();
+
+            if ($customer) {
+
+                return response()->json(['status' => true, 'message' => 'customer add success'])->setStatusCode(200);
+            }
+            return response()->json(['status' => false, 'message' => 'customer add failed'])->setStatusCode(400);
+        } catch (Exception $exception) {
+            return response()->json(['status' => false, 'message' => 'public.internal_server_error'])->setStatusCode(500);
         }
     }
 }
