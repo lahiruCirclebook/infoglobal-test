0f932519b1e3dbe1ebe5f6d945e1095113a1941b
lahiruCirclebook <lahirucj98@gmail.com>
2023-02-05 23:48:23 +0530

    changes

--- frontend/login.php
+++ frontend/login.php
@@ -4,6 +4,10 @@
 <head>
     <?php include 'Layout/loginLayout.php'; ?>
 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -11,7 +15,149 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
+<style>
+    *,
+    *:before,
+    *:after {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: #080710;
+    }
+
+    .background {
+        width: 430px;
+        height: 520px;
+        position: absolute;
+        transform: translate(-50%, -50%);
+        left: 50%;
+        top: 50%;
+    }
+
+    .background .shape {
+        height: 200px;
+        width: 200px;
+        position: absolute;
+        border-radius: 50%;
+    }
+
+    .shape:first-child {
+        background: linear-gradient(#1845ad,
+                #23a2f6);
+        left: -80px;
+        top: -80px;
+    }
+
+    .shape:last-child {
+        background: linear-gradient(to right,
+                #ff512f,
+                #f09819);
+        right: -30px;
+        bottom: -80px;
+    }
+
+    form {
+        height: 520px;
+        width: 400px;
+        background-color: rgba(255, 255, 255, 0.13);
+        position: absolute;
+        transform: translate(-50%, -50%);
+        top: 50%;
+        left: 50%;
+        border-radius: 10px;
+        backdrop-filter: blur(10px);
+        border: 2px solid rgba(255, 255, 255, 0.1);
+        box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
+        padding: 50px 35px;
+       
+    }
+
+    form * {
+        font-family: 'Poppins', sans-serif;
+        color: #ffffff;
+        letter-spacing: 0.5px;
+        outline: none;
+        border: none;
+    }
+
+    form h3 {
+        font-size: 32px;
+        font-weight: 500;
+        line-height: 42px;
+        text-align: center;
+    }
+
+    label {
+        display: block;
+        margin-top: 30px;
+        font-size: 16px;
+        font-weight: 500;
+    }
+
+    input {
+        display: block;
+        height: 50px;
+        width: 100%;
+        background-color: rgba(255, 255, 255, 0.07);
+        border-radius: 3px;
+        padding: 0 10px;
+        margin-top: 8px;
+        font-size: 14px;
+        font-weight: 300;
+    }
+
+    ::placeholder {
+        color: #e5e5e5;
+    }
+
+    button {
+        margin-top: 50px;
+        width: 100%;
+        background-color: #03001C;
+        color: #080710;
+        padding: 15px 0;
+        font-size: 18px;
+        font-weight: 600;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    .social {
+        margin-top: 30px;
+        display: flex;
+    }
+
+    .social div {
+        background: red;
+        width: 150px;
+        border-radius: 3px;
+        padding: 5px 10px 10px 5px;
+        background-color: rgba(255, 255, 255, 0.27);
+        color: #eaf0fb;
+        text-align: center;
+    }
+
+    .social div:hover {
+        background-color: rgba(255, 255, 255, 0.47);
+    }
+
+    .social .fb {
+        margin-left: 25px;
+    }
+
+    .social i {
+        margin-right: 4px;
+    }
+</style>
+
 <body class="g-sidenav-show  bg-gray-100">
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
     <section class="min-vh-100 mb-8">
         <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg" style="background-image: url('assets/img/curved-images/curved14.jpg');">
             <span class="mask bg-gradient-dark opacity-6"></span>
@@ -27,12 +173,11 @@
         <div class="container">
             <div class="row mt-lg-n10 mt-md-n11 mt-n10">
                 <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
-                    <div class="card z-index-0">
-                        <div class="card-header text-center pt-4">
-                            <h5>Login</h5>
-                        </div>
+                    <div class="">
+                       
                         <div class="card-body">
                             <form id="login">
+                                <br><br>
                                 <div class="mb-3">
                                     <input type="text" class="form-control" placeholder="Email ID" name="email">
                                 </div>
@@ -43,7 +188,7 @@
                                     <button type="submit" class="btn btn-primary w-100 my-4 mb-2">Login Now</button>
                                 </div>
                                 <center>
-                                    <p class="text-sm mt-3 mb-0">Create New Account? <a href="register.php" class="text-dark font-weight-bolder">register</a></p>
+                                    <p class="text-sm mt-3 mb-0">Create New Account? <a href="register.php" class="text-white font-weight-bolder"><p>Register</p></a></p>
                                 </center>
 
                             </form>
